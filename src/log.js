ebb22940c69470d1e5dabda3c9e899843313ac4a
Zhenfei.Li <Zhenfei.Li@fanruan.com>
2020-03-03 16:26:44 +0800

    feat: 让小舔舔每天5点提醒大家记日志

---
+++ src/log.js
@@ -0,0 +1,44 @@
+const request = require('request')
+
+const CONFIG = require('./config.js').config
+
+const send_msg = (callback) => {
+  request({
+    json: true,
+    method: 'POST',
+    url: CONFIG.URL,
+    body: {
+        'msgtype': 'text',
+        'text': {
+            'content': '小哥哥，工作辛苦啦！不要忘了今天的工作日志哦ヾ(◍°∇°◍)ﾉﾞ',
+            'mentioned_list': ['@all']
+        }
+    }
+  }, (err, res, body) => {
+    typeof callback === 'function' && callback(err, res, body)
+  })
+}
+
+let counter = 0
+let timer = null
+
+const callback = (err, res, body) => {
+    counter += 1
+    if (err && counter <= CONFIG.MAX_TIMES) {
+    if (counter <= CONFIG.MAX_TIMES) {
+        console.log('suck failed\nauto retry...')
+    } else {
+        console.log('suck faied 10 times, please check your network and retry later.')
+        clearInterval(timer)
+    }
+    return
+    }
+    clearInterval(timer)
+    console.log('suck success')
+}
+
+timer = setInterval(() => {
+    send_msg(callback)
+}, CONFIG.TIME_GAP)
+
+send_msg(callback)
